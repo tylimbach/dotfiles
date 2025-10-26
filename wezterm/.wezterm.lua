090e1a8d5e26ca704cf8365915f77788b8b8ca54
Tyler Limbach <tylimbach@gmail.com>
2025-10-26 10:57:15 -0500

    wezterm: pane zoom toggle

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,109 +2,23 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- config.font = wezterm.font({
--- 	-- family = "Iosevka Term Slab",
--- 	-- family = "CaskaydiaCove Nerd Font",
--- 	-- family = "MonoLisa",
--- 	-- family = "Berkeley Mono",
--- 	-- family = "Monaspace Argon",
--- 	-- family = "Monaspace Neon",
--- 	-- family = "DejaVuSansM Nerd Font",
--- 	-- family = "CommitMono",
--- 	-- family = "Operator Mono",
--- 	-- family = "Operator Mono Lig",
--- 	-- family = "FiraCode Nerd Font",
--- })
-
 config.wsl_domains = {
 	{
 		name = "WSL:Ubuntu",
 		distribution = "Ubuntu-22.04",
 	},
 }
--- config.default_domain = "WSL:Ubuntu"
 
 config.font = wezterm.font_with_fallback {
 	'Operator Mono Medium',
+	'Operator Mono Book',
 	'Operator Mono Bold',
 	'Operator Mono Lig',
+	'CaskaydiaCove Nerd Font',
 	'UnifontExMono',
 	'JuliaMono',
 	-- 'Berkeley Mono',
 	-- 'Iosevka Nerd Font',
-	{
-		family = 'MonoLisa',
-		harfbuzz_features = {
-			-- liga - Coding ligatures
-			"liga=0",
-
-			-- frac - Fractions, turns 1/2 into ½
-			-- "frac",
-
-			-- onum - Old style numbers
-			-- "onum",
-
-			-- calt - Whitespace ligatures
-			"calt",
-
-			-- zero - Slashed zero
-			-- "zero",
-
-			-- ss01 - Normal asterisk
-			-- "ss01",
-
-			-- ss02 - Italic Script Version
-			-- "ss02",
-
-			-- ss03 - Alternative g
-			-- "ss03",
-
-			-- ss04 - Other alternative g
-			-- "ss04",
-
-			-- ss05 - Alternative ß
-			-- "ss05",
-
-			-- ss06 - Alternative @
-			-- "ss06",
-
-			-- ss07 - Alternative curly brackets {}
-			-- "ss07",
-
-			-- ss08 - Alternative parenthesis ()
-			-- "ss08",
-
-			-- ss09 - Alternative >= ligature
-			-- "ss09",
-
-			-- ss10 - Other alternative >= ligature
-			-- "ss10",
-
-			-- ss11 - Centered hexadecimal x 0xF
-			-- "ss11",
-
-			-- ss12 - Thin backslash
-			-- "ss12",
-
-			-- ss13 - Alternative $
-			-- "ss13",
-
-			-- ss14 - Alternative &
-			-- "ss14",
-
-			-- ss15 - i without serif
-			-- "ss15",
-
-			-- ss16 - r without serif
-			-- "ss16",
-
-			-- ss17 - Alternative .= and ..= ligature
-			-- "ss17",
-
-			-- ss18 - Alternative @
-			-- "ss18",
-		},
-	},
 	{
 		family = 'Iosevka Nerd Font',
 		harfbuzz_features = { "liga", "calt", "ss03" },
@@ -158,6 +72,11 @@ config.keys = {
 		key = ' ',
 		mods = 'CTRL',
 		action = wezterm.action.SendKey { key = ' ', mods = 'CTRL' },
+	},
+	{
+		key = 'z',
+		mods = 'CTRL',
+		action = wezterm.action.TogglePaneZoomState,
 	}
 }
 
